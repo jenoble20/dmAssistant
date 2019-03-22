725e001f0c38a82d36fcafb20cbdef6ecf81df92
jenoble <jenoble20@gmail.com>
2019-03-21 21:49:12 -0400

    Finished base functionality for displaying the modifier for each stat.

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,70 +1,69 @@
 $(document).ready(function(){
     let value = 0;
 
-    //$(this) IS REFERRING TO THE ENTIRE DOCUMENT, NO THE ELEMENT THAT IS BEING INTERACTED WITH 
-    $(".stat").mouseenter(()=>{
-        value = $(this).text();
-        let modifier = getMod(value);
-        console.log($(this))
-        console.log(value);
-        console.log(modifier);
-        $(this).children(".value").text(value)
-    })
-    $(".stat").mouseleave(()=>{
-        $(this).text(value);
+    $(".stat").mouseenter(function(){
+        value = $(this).children(".value").text();
+        value = parseInt(value, 10);
+        $(this).children(".value").text(getMod(value));
+    })//END MOUSEENTER EVENT
+    $(".stat").mouseleave(function(){
+        $(this).children(".value").text(value);
         value = 0;
-    })
+    })// END MOUSELEAVE EVENT
 
-    const getMod = function(value){
-        let mod = -10
-        switch(value){
-            case 1:
-                mod= -1;
+    const getMod = function(val){
+        let mod = -20
+        switch(true){
+            case (val==1):
+                mod = -1;
             break;
-            case (2<=value<=3):
-                mod= -4; 
+            case (2<=val && val<=3):
+                mod = -4; 
             break;
-            case (4<=value<=5):
-                mod= -3;
+            case (4<=val && val<=5):
+                mod = -3;
             break;
-            case (6<=value<=7):
-                mod= -2;
+            case (6<=val && val<=7):
+                mod = -2;
             break;
-            case (8<=value<=9):
-                mod= -1;
+            case (8<=val && val<=9):
+                mod = -1;
             break;
-            case (10<=value<=11):
-                mod= 0
+            case (10<=val && val<=11):
+                mod = 0
             break;
-            case (12<=value<=13):
-                mod= 1;
+            case (12<=val && val<=13):
+                mod = 1;
             break;
-            case (14<=value<=15):
-                mod= 2;
+            case (14<=val && val<=15):
+                mod = 2;
             break;
-            case (16<=value<=17):
-                mod= 3;
+            case (16<=val && val<=17):
+                mod = 3;
             break;
-            case (18<=value<=19):
-                mod= 4;
+            case (18<=val && val<=19):
+                mod = 4;
             break;
-            case (20<=value<=21):
-                mod= 5;
+            case (20<=val && val<=21):
+                mod = 5;
             break;
-            case (22<=value<=23):
-                mod= 6;
+            case (22<=val && val<=23):
+                mod = 6;
             break;
-            case (24<=value<=25):
-                mod= 7;
+            case (24<=val && val<=25):
+                mod = 7;
             break;
-            case (26<=value<=27):
-                mod= 8;
+            case (26<=val && val<=27):
+                mod = 8;
             break;
-            case (28<=value<=29):
-                mod= 9;
+            case (28<=val && val<=29):
+                mod = 9;
             break;
-            case 30:
-                mod= 10;
+            case (val==30):
+                mod = 10;
+            break;
+            default:
+                console.log("NOT FOUND");
             break;
         }
         return mod;
